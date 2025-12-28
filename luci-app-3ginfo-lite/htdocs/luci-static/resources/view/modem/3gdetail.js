b59c89e787afb7c423f15b2639953442fdc639f0
Rafał Wabik <4rafal@gmail.com>
2025-12-28 23:21:49 +0100

    Adding a dedicated loading animation

--- luci-app-3ginfo-lite/htdocs/luci-static/resources/view/modem/3gdetail.js
+++ luci-app-3ginfo-lite/htdocs/luci-static/resources/view/modem/3gdetail.js
@@ -548,7 +548,7 @@ simDialog: baseclass.extend({
 				}
 					
 					var icon, wicon, ticon, t;
-					var wicon = L.resource('icons/loading.gif');
+					var wicon = L.resource('icons/3ginfo-load.gif');
 					var ticon = L.resource('icons/ctime.png');
 
 					var p = (json.signal);
